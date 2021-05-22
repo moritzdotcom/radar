b8d065ef142bb1af33334f47c56da264fb50a995
moritzdotcom <moritzdotcom@users.noreply.github.com>
2021-05-22 09:39:37 +0000

    deploy: 4d8304e57e00de103dfdcda97f38a1d1dcc8a74b

---
+++ _nuxt/static/1621676376/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fradar\u002F_nuxt\u002Fstatic\u002F1621676376",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnewsletter",config:{app:{basePath:"\u002Fradar\u002F",assetsPath:"\u002Fradar\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"13b9c9b7"}},globalRefs:{}}}(null));
